ceda144790283100a988dc0d1450b0729de2602e
Devanand-PA <devanandpa721@gmail.com>
2025-09-14 17:04:50 +0530

    added_minecraft

---
+++ minecraft/fishing/auto-fisher.py
@@ -0,0 +1,22 @@
+#!/bin/python3
+import pyautogui
+import pytesseract
+from PIL import ImageGrab
+import time
+import subprocess
+
+while True:
+    # grab a region of the screen
+    screenshot = ImageGrab.grab(bbox=(1430, 630, 1980, 1080))  # adjust coordinates
+    text = pytesseract.image_to_string(screenshot)
+
+    print(text)
+    if "obber splashes" in text:
+        # pyautogui.mouseDown(button="left")
+        # pyautogui.mouseUp(button="left")
+        subprocess.run("xdotool click 3",shell=True)
+        time.sleep(0.5)
+        subprocess.run("xdotool click 3",shell=True) 
+        time.sleep(3)
+    time.sleep(0.5)
+
